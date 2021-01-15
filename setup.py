0a9ab4a41ddeedc0dc40c17e0634b2170c2a3aee
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-15 10:59:11 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+NAME = "thing-NAV"
+URL = "NONE"
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+    name=NAME,
+    version='0.0.1a1',
+    description='none',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=['code'],
+    author='NAV',
+    author_email='skillfulhacks@gmail.com',
+    url=URL
+)
