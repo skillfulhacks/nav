fdccafea629988e51868e60b6ad786e5ea867d81
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-19 09:47:46 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 19 08:51:35 2021
-
-@author: clarence
-"""
 import yaml
 from <<<ADD YOU MODULE HERE>>> import <<ADD YOUR FUNCTION HERE>>>
 
