9f4363a85a591b27f83f05125f007d2cb12fa1c3
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-19 09:20:27 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -8,12 +8,13 @@ Created on Tue Jan 19 08:51:35 2021
 import yaml
 import <<<ADD YOU MODULE HERE>>>
 
-
+### Load Config File ###
 CONFIGPATH = "xvrc.yaml"
 
 with open("xvrc.yaml") as file:
     CFGFILE = yaml.load(file, Loader=yaml.FullLoader)
-    
+
+### If Program Has Never Been Run Before Update the onfig File ###
 if CFGFILE["internal"]["cfg_file_built"] == 0:
     from set_yaml import UpdateYaml
     UpdateYaml("xvrc.yaml")
