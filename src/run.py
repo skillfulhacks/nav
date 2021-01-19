f4a19d398e2602dcba22f0b8cf8ee4fbbe313785
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-19 12:24:16 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import yaml
-from <<<ADD YOU MODULE HERE>>> import <<ADD YOUR FUNCTION HERE>>>
+from XV_File_Manager import MainFunc
 
 ### Load Config File ###
-CONFIGPATH = "xvrc.yaml"
+CONFIGPATH = ".config/xvrc.yaml"
 
-with open("xvrc.yaml") as file:
+with open(CONFIGPATH) as file:
     CFGFILE = yaml.load(file, Loader=yaml.FullLoader)
 
 ### If Program Has Never Been Run Before Update the onfig File ###
@@ -15,4 +15,4 @@ if CFGFILE["internal"]["cfg_file_built"] == 0:
     UpdateYaml("xvrc.yaml")
     
     
-<<<YOUR FUNCTION>>>(CFGFILE)
+MainFunc(CFGFILE)
