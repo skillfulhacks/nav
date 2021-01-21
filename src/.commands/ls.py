ca8e98fdfe402da3777ab6f1abd5ee0a30b6ad90
X3d111 <76403149+X3d111@users.noreply.github.com>
2021-01-21 15:10:30 -0400

    Update ls.py

--- src/.commands/ls.py
+++ src/.commands/ls.py
@@ -3,7 +3,13 @@
 
 def func(root, *args, **kwargs):
     import os
+    root["tkconsole"].insert("end","Listing all files in XV's current set directory:\n")
     for item in os.listdir(root["cwd"]):
-        root["tkconsole"].insert("end", f"\n{item}")
-    
+        try:
+            os.chdir(item)
+            root["tkconsole"].insert("end", f"\nFolder: {item}")
+            os.chdir("..")
+        except NotADirectoryError:
+            root["tkconsole"].insert("end", f"\nFile: {item}")
+            
 doc = """Lists CWD"""
