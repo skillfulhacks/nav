9003dd25ff32647c89021e7b745f971e4adedf59
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-21 15:33:33 -0400

    Update man.py

--- src/.commands/man.py
+++ src/.commands/man.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Displays Help For Selected Command; Usage - man <command>"""
 
 def func(root, *args, **kwargs):
-    root["tkconsole"].insert("end", root["imported_commands"][args[0]].doc)
+    root["tkconsole"].insert("end", root["imported_commands"][args[0]].__doc__)
     
-doc = """Displays Help For Selected Command; Usage - man <command>"""
