8eb2d3f681dc7c40c7d2e4fa97a272b3b882cd9a
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-21 15:40:33 -0400

    Update XV_File_Manager.py

--- src/XV_File_Manager.py
+++ src/XV_File_Manager.py
@@ -5,29 +5,20 @@ import sys
 
 import yaml
 
-
 import tkinter as tk
-from tkinter import ttk
 
+from tk_custum_widgets import create_textbox, TKConsole, ScrolledFrame
 from console import Console
+
 """---IMPORTANT VARIABLES---
 BACE_PATH <--- The path that this file is placed in
 PLATFORM <--- Users platform
 SLASH <--- The Slash type used in users paths "\\" or "/""
 CWD <--- The set path stands for 'current working directory'
 """
-def create_textbox(root, values, gx, gy, gs="nswe", **kwargs):
-        """Creates and Grids A Combobox"""
-        box = ttk.Combobox(root, values=values, **kwargs)
-        box.grid(row=gx, column=gy, sticky=gs)
-        box.set(values[0])
-        
-        return box
 
-
-
-    
-class xv_files():
+          
+class XVFiles():
     def chdir(self, newdir):
         os.chdir(newdir)
         self.cwd = newdir
@@ -66,51 +57,39 @@ class xv_files():
         
         # Create a tkinter screen named self.tkroot
         self.tkroot = tk.Tk()
-        #self.tkroot.geometry('500x400')
+        self.tkroot.resizeable(False, False)
         self.tkroot.title("XV File Manager")
         
-        self.tkcommand_frame = tk.Frame(self.tkroot)
+        self.tkcommand_frame = ScrolledFrame(self.tkroot)
         self.tkcommand_frame.grid(column=0, row=0)
         
-        # Stop window resizability
-        self.tkroot.resizable(False,False)
-        
         # Creates Console
-        # BTW I dont really know how this works
         self.tkconsole_frame = tk.Frame(self.tkroot)
         self.tkconsole_frame.grid(column=1, row=0, rowspan=100)
         
-        # Scroll Bars
-        self.tkconsole_framehscroll = tk.Scrollbar(self.tkconsole_frame, orient='horizontal')
-        self.tkconsole_framehscroll.pack(side=tk.BOTTOM, fill=tk.X)
-        self.tkconsole_framevscroll = tk.Scrollbar(self.tkconsole_frame)
-        self.tkconsole_framevscroll.pack(side=tk.RIGHT, fill=tk.Y)
-        
-        self.tkconsole = tk.Text(self.tkconsole_frame, width=35, height=25, wrap=tk.NONE,
-                    xscrollcommand=self.tkconsole_framehscroll.set,
-                    yscrollcommand=self.tkconsole_framevscroll.set, font='monospace 10')
+        self.tkconsole = TKConsole(self.tkconsole_frame, width=80, height=24, wrap=tk.NONE, font='monospace 10')
         self.tkconsole.pack(side=tk.TOP, fill=tk.X)
-        self.tkconsole_framehscroll.config(command=self.tkconsole.xview)
-        self.tkconsole_framevscroll.config(command=self.tkconsole.yview)
-        
+
         # Create a Entrybox with CWD inserted
-        tk.Label(self.tkcommand_frame,text="set directory: ",font="monospace 8").grid(row=0,column=0)
-        set_dir_entry = create_textbox(self.tkcommand_frame, [self.root_path], 0, 1, font="monospace 8", width=60)
+        tk.Label(self.tkcommand_frame.interior, text="set directory: ",font="monospace 8").grid(row=0,column=0)
+        set_dir_entry = create_textbox(self.tkcommand_frame.interior, [self.root_path], 0, 1, font="monospace 8", width=60)
     
-        tk.Label(self.tkcommand_frame,text="Args:",font="monospace 8").grid(row=1,column=0)
-        self.tkargs_combobox = create_textbox(self.tkcommand_frame, [""], 1, 1, font="monospace 8", width=60)
+        tk.Label(self.tkcommand_frame.interior, text="Args:",font="monospace 8").grid(row=1,column=0)
+        self.tkargs_combobox = create_textbox(self.tkcommand_frame.interior, [""], 1, 1, font="monospace 8", width=60)
         # Main Button Loop
         for i, command in enumerate(self.command_files):
-            tk.Label(self.tkcommand_frame,text=command,font="monospace 10",fg="darkblue").grid(row=2,column=i * 3,sticky="nswe")
-            tk.Button(self.tkcommand_frame,text=f"Run {command}", font="monospace 9", command=lambda x=command:self.run_command(x)).grid(row=2,column=i * 4 + 1 ,sticky="nswe")
+            tk.Label(self.tkcommand_frame.interior,text=command,font="monospace 10",fg="darkblue").grid(row=2 + i,column=0,sticky="nswe")
+            tk.Button(self.tkcommand_frame.interior,text=f"Run {command}", font="monospace 9", command=lambda x=command:self.run_command(x)).grid(row=2 + i,column=1 ,sticky="nswe")
             
             
         # self.tkroot mainloop
         self.tkroot.mainloop()
     
-    def run_command(self, command):        
+    def run_command(self, command):
+        print(command)        
+        
         try:
-            self.tkconsole.insert("end", f"\n\nRunCommmand {command} - ARGS: {self.tkargs_combobox.get()} \n")
+            self.tkconsole.insert("end", f"\nIN: run_command {command} - ARGS: {self.tkargs_combobox.get()} \nOUT:\n")
             self.imported_commands[command].func(self.__dict__, *self.tkargs_combobox.get().split(" "))
         except KeyError:
             self.pyconsole.print(f"Invailed Command {command}", level=2)
@@ -118,4 +97,4 @@ class xv_files():
 if __name__ == "__main__":
     with open("xvrc.yaml") as file:
         YAMLCFG = yaml.load(file, Loader=yaml.FullLoader)
-    xv_files(YAMLCFG)
+    XVFiles(YAMLCFG)
