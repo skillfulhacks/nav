3677ebc8323b8bdf367bda36810677e8e7eaef58
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-22 07:04:11 -0400

    Update XV_File_Manager.py

--- src/XV_File_Manager.py
+++ src/XV_File_Manager.py
@@ -6,18 +6,12 @@ import sys
 import yaml
 
 import tkinter as tk
+from tkinter import ttk
 
 from tk_custum_widgets import create_textbox, TKConsole, ScrolledFrame
 from console import Console
 
-"""---IMPORTANT VARIABLES---
-BACE_PATH <--- The path that this file is placed in
-PLATFORM <--- Users platform
-SLASH <--- The Slash type used in users paths "\\" or "/""
-CWD <--- The set path stands for 'current working directory'
-"""
-
-          
+     
 class XVFiles():
     def chdir(self, newdir):
         os.chdir(newdir)
@@ -60,15 +54,13 @@ class XVFiles():
         self.tkroot.title("XV File Manager")
         self.tkroot.resizable(False, False)
         
-        self.tkcommand_frame = ScrolledFrame(self.tkroot)
+        ### Input Frame ###
+        self.tkinput_frame_tabs = ttk.Notebook(self.tkroot)
+        self.tkinput_frame_tabs.grid(row=0, column=0)
+        
+        ## Command Tab ##
+        self.tkcommand_frame = ScrolledFrame(self.tkinput_frame_tabs)
         self.tkcommand_frame.grid(column=0, row=0)
-        
-        # Creates Console
-        self.tkconsole_frame = tk.Frame(self.tkroot)
-        self.tkconsole_frame.grid(column=1, row=0, rowspan=100)
-        
-        self.tkconsole = TKConsole(self.tkconsole_frame, width=80, height=24, wrap=tk.NONE, font='monospace 10')
-        self.tkconsole.pack(side=tk.TOP, fill=tk.X)
 
         # Create a Entrybox with CWD inserted
         tk.Label(self.tkcommand_frame.interior, text="set directory: ",font="monospace 8").grid(row=0,column=0)
@@ -83,11 +75,31 @@ class XVFiles():
         for i, command in enumerate(self.command_files):
             args_entry = create_textbox(self.tkcommand_frame.interior, [""], font="monospace 8")
             args_entry.grid(row=2 + i, column=3, sticky="ew")
-            tk.Button(self.tkcommand_frame.interior,text=f"{command}", font="monospace 9", command=lambda entry=args_entry, x=command:self.run_command(x, entry)
+            tk.Button(self.tkcommand_frame.interior,text=f"{command}", font="monospace 9 bold", command=lambda entry=args_entry, x=command:self.run_command(x, entry)
                       ).grid(row=2 + i, column=0, columnspan=3, sticky="nswe")
+        
+        ## Term Tab ##
+        self.tkinputconsole_frame = tk.Frame(self.tkinput_frame_tabs)
+        self.tkinputconsole_frame.grid(column=0, row=0, rowspan=100)
+        
+        self.tkinputconsole = TKConsole(self.tkinputconsole_frame, width=80, height=24, wrap=tk.NONE, font='monospace 10')
+        self.tkinputconsole.pack(side=tk.TOP, fill=tk.X)
+        
+        ### Output Frame ###
+        self.tkoutput_frame = ttk.Frame(self.tkroot)
+        self.tkoutput_frame.grid(row=0, column=1)
             
-            
+        # Creates Console
+        self.tkconsole_frame = tk.Frame(self.tkoutput_frame)
+        self.tkconsole_frame.grid(column=1, row=0, rowspan=100)
+        
+        self.tkconsole = TKConsole(self.tkconsole_frame, width=80, height=24, wrap=tk.NONE, font='monospace 10', bg="black")
+        self.tkconsole.pack(side=tk.TOP, fill=tk.X)
+        
+        
         # self.tkroot mainloop
+        self.tkinput_frame_tabs.add(self.tkcommand_frame, text="Commands")     
+        self.tkinput_frame_tabs.add(self.tkinputconsole_frame, text="  Term  ")
         self.tkroot.mainloop()
     
     def run_command(self, command, entry):
