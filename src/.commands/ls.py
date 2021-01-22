1ecd04e4489b6ab7dc2059981bef95a8a8bd020e
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-22 15:06:45 -0400

    Update ls.py

--- src/.commands/ls.py
+++ src/.commands/ls.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__doc__ = """This lists all files in XV's current set directory."""
+
+"""Lists CWD - Usage: ls"""
 
 def func(root, *args, **kwargs):
     import os
-    root["tkconsole"].insert("end","Listing all files in XV's current set directory...\n")
     for item in os.listdir(root["cwd"]):
-        try:
-            os.chdir(item)
-            root["tkconsole"].insert("end", f"Folder: {item}")
-            os.chdir("..")
-        except NotADirectoryError:
+        if os.path.isdir(f"{root['cwd']}/{item}"):
+            root["tkconsole"].insert("end", f"{item} (dir)")
+        else:
             root["tkconsole"].insert("end", f"File: {item}")
-   
-            
