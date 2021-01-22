6754821d87af1b68674901fc76c9472515b6e1bb
X3d111 <76403149+X3d111@users.noreply.github.com>
2021-01-22 09:33:29 -0400

    Update sysinfo.py

--- src/.commands/sysinfo.py
+++ src/.commands/sysinfo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+__doc__ = """Lists divice info."""
 
 def func(root, *args, **kwargs):
     import platform as pf
     root["tkconsole"].insert("end","Listing divice info...")
     root["tkconsole"].insert("end",f"Current OS:   {pf.system()}\nProcessor:   {pf.processor()}\nMachine:   {pf.machine()}\nArchitecture:   {pf.architecture()}\nNetwork:   {pf.node()}")
     root["tkconsole"].insert("end","Done!")
-            
-__doc__ = """Lists divice info."""
+         
