3feecaa4c658b83e4fceb2146c4ce337100dd85e
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-26 18:24:10 -0400

    Create cd.py

---
+++ src/.commands/cd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Changes CWD - Usage: cd <path>"""
+
+def func(root, *args, **kwargs):
+    import os
+    try:
+        path = args[0]
+        restore = root["cwd"]
+    
+        if root["slash"] in (path[0], path[0:1]):
+            root["cwd"] = path
+        elif path == "..":
+            root["cwd"] = f'{root["slash"].join(root["cwd"].split(root["slash"])[:-1])}'
+        else:
+            root["cwd"] = f'{root["cwd"]}/{path}'
+            
+        if os.path.isdir(root["cwd"]):
+            root["tkconsole"].insert("end", f"Path Changed to {root['cwd']}")
+        else:
+            root["tkconsole"].insert("end", f"Unknown Path {root['cwd']}")
+            root["cwd"] = restore
+    except IndexError:
+        root["cwd"] = root["root_dir"]
+    
