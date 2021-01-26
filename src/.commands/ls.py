cd61c116617122a9f6463e45e21faa06ef61792d
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-26 18:22:30 -0400

    Update ls.py

--- src/.commands/ls.py
+++ src/.commands/ls.py
@@ -6,7 +6,8 @@
 def func(root, *args, **kwargs):
     import os
     for item in os.listdir(root["cwd"]):
+        root["tkconsole"].insert("end", item)
         if os.path.isdir(f"{root['cwd']}/{item}"):
-            root["tkconsole"].insert("end", f"{item} (dir)")
+            root["tkconsole"].insert("end", f"{''.join([' ' for ch in range(30) if len(item) + ch < 30])}(dir)", sep="")
         else:
-            root["tkconsole"].insert("end", f"File: {item}")
+            root["tkconsole"].insert("end", f"{''.join([' ' for ch in range(30) if len(item) + ch < 30])}(file)", sep="")
