a33c2158139000e72369c70f6b89b20657474e72
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-26 18:27:27 -0400

    Update command_ls.py

--- src/gui_tabs/command_ls.py
+++ src/gui_tabs/command_ls.py
@@ -8,19 +8,18 @@ NAME = "Commands"
 class Tab(ScrolledFrame):
     def __init__(self, root, tkroot): 
         ScrolledFrame.__init__(self, tkroot)
-
-
+        self.root = root
+        
         # Create a Entrybox with CWD inserted
         tk.Label(self.interior, text="set directory: ",font="monospace 8").grid(row=0,column=0)
-        set_dir_entry = create_textbox(self.interior, [root["cwd"]], font="monospace 8", width=60)
-        set_dir_entry.grid(row=0, column=1, columnspan=3)
+        self.set_dir_entry = create_textbox(self.interior, [root["cwd"]], font="monospace 8", width=60, state="readonly")
+        self.set_dir_entry.grid(row=0, column=1, columnspan=3)
         
         tk.Label(self.interior, text="Command",font="monospace 16").grid(row=1, column=0, columnspan=2, sticky="w")
         tk.Label(self.interior, text="Args",font="monospace 16").grid(row=1, column=2, columnspan=2, sticky="ew")
 
-        
         # Main Button Loop
-        for i, command in enumerate(root["command_files"]):
+        for i, command in enumerate(root["imported_commands"].keys()):
             try:
                 if root["imported_commands"][command].INTERNAL:
                     continue
@@ -30,3 +29,7 @@ class Tab(ScrolledFrame):
             args_entry.grid(row=2 + i, column=3, sticky="ew")
             tk.Button(self.interior,text=f"{command}", font="monospace 9 bold", command=lambda entry=args_entry, x=command:root["run_command"](root, x, entry.get().split(" "))
                       ).grid(row=2 + i, column=0, columnspan=3, sticky="nswe")
+    
+    def update(self):
+        """Updates the self.set_dir_entry When a Command is run"""
+        self.set_dir_entry.set(self.root["cwd"])
