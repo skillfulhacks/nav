3e71c3a5c9b6e3896081ab05695484c626515dca
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-27 16:43:59 -0400

    Delete src directory

--- src/commands/man.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""Displays Help For Selected Command; Usage - man <command>"""
-
-def func(root, display, *args, **kwargs):
-    display.insert("end", root["imported_commands"][args[0]].__doc__, sep="")
-    
-    
