78fb23ada3e8a33090a2fc5edfecfee8167c9165
skillfulhacks <67869615+skillfulhacks@users.noreply.github.com>
2021-01-27 13:50:42 -0400

    Delete src directory

--- src/.commands/man.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""Displays Help For Selected Command; Usage - man <command>"""
-
-def func(root, *args, **kwargs):
-    root["tkconsole"].insert("end", root["imported_commands"][args[0]].__doc__, sep="")
-    
